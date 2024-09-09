10aa99195c2b27f91806ee2b5c15b89f23b3a802
Foodlens1213 <foodlens1213@gmail.com>
2024-09-09 22:38:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,6 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    port = int(os.getenv("PORT", 5000))  # Render will provide the PORT env variable
+    app.run(host="0.0.0.0", port=port)
+
