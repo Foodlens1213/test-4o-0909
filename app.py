8d155231792f81105b1a59b8734c5687096e2ec7
Foodlens1213 <foodlens1213@gmail.com>
2024-10-16 14:44:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv('MYSQL_HOST'),
         user=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASSWORD'),
         database=os.getenv('MYSQL_DATABASE')
     )
 
