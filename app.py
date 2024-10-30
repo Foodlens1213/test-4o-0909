9d96f4894f6724f4c23f4c1c6a38f90a7273deed
Foodlens1213 <foodlens1213@gmail.com>
2024-10-31 05:25:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ import re
 def clean_text(text):
     # 去除無效字符和表情符號
     return re.sub(r'[^\w\s,.!?]', '', text)
-def create_flex_message(recipe_text, user_id, dish_name, ingredients, recipe_number):
+def create_flex_message(recipe_text, user_id, dish_name, ingredient_text, ingredients, recipe_number):
     recipe_id = save_recipe_to_db(user_id, dish_name, recipe_text)
     if isinstance(ingredients, list):
         ingredients_str = ','.join(ingredients)
