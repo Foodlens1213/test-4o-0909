2603b515dd5253648c2844626abf03cecece0efb
Foodlens1213 <foodlens1213@gmail.com>
2024-11-23 03:03:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def create_flex_message(recipe_text, user_id, dish_name, ingredient_text, ingred
             "contents": [
                 {"type": "button", "action": {"type": "postback", "label": "有沒有其他的食譜", "data":f"action=new_recipe&user_id={user_id}&ingredients_id={ingredients_id}"},
                  "color": "#474242", "style": "primary", "height": "sm"},
-                {"type": "button", "action": {"type": "postback", "label": "把這個食譜加入我的最愛", "data":f"action=save_favorite&recipe_id={recipe_id}",
+                {"type": "button", "action": {"type": "postback", "label": "把這個食譜加入我的最愛", "data":f"action=save_favorite&recipe_id={recipe_id}"},
                  "color": "#474242", "style": "primary", "height": "sm"}
             ]
         }
